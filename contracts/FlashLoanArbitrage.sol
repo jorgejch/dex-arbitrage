76e51027e44f91d83fbc826933ed677dfa8fc71d
Jorge Haddad <jorgejch@gmail.com>
2024-11-17 02:32:40 -0800

    Update Solidity version to 0.8.10 and add Hardhat console for debugging

--- contracts/FlashLoanArbitrage.sol
+++ contracts/FlashLoanArbitrage.sol
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.21;
+pragma solidity ^0.8.10;
 
 import "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
 import "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
 import "@aave/core-v3/contracts/interfaces/IPool.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+
+import "hardhat/console.sol";
 
 /**
  * @title Flash-Loan Arbitrage Contract
