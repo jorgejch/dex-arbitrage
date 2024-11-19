942bd1ec15e0d98d215e9758875f67b68292038b
Jorge Haddad <jorgejch@gmail.com>
2024-11-19 01:04:04 -0800

    Update FlashLoanArbitrage contract deployment to include additional constructor argument for improved functionality

--- ignition/modules/aflab_testnet.ts
+++ ignition/modules/aflab_testnet.ts
@@ -2,7 +2,7 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules"
 
 export default buildModule("aflab_testnet", (m) => {
   // Deploy the FlashLoanArbitrage contract with the Sepolia testnet Pool Address Provider address as the constructor argument
-  const flashLoanArbitrage = m.contract("FlashLoanArbitrage", ["0x012bAC54348C0E635dCAc9D5FB99f06F24136C9A"])
+  const flashLoanArbitrage = m.contract("FlashLoanArbitrage", ["0x012bAC54348C0E635dCAc9D5FB99f06F24136C9A", "0x1b81D678ffb9C0263b24A97847620C99d213eB14"])
 
   return { flashLoanArbitrage }
 })
