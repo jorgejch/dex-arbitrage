688e3d3086f9910ddff5c350a633b10bed813e40
Jorge Haddad <jorgejch@gmail.com>
2024-11-19 21:05:26 -0800

    Add fetchLiquidityInfo function to retrieve liquidity information for token pairs

---
+++ src/helper.ts
@@ -0,0 +1,6 @@
+async function fetchLiquidityInfo(tokenPair: string): Promise<any> {
+  // Logic to fetch liquidity information for the specified token pair
+  return {};
+}
+
+export { fetchLiquidityInfo };
