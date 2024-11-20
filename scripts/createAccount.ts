9576d3cfe7fd6c70ece82b13bc5e00fcf8b5c252
Jorge Haddad <jorgejch@gmail.com>
2024-11-19 18:13:14 -0800

    Refactor createAccount script to improve readability and add documentation

--- scripts/createAccount.ts
+++ scripts/createAccount.ts
@@ -1,11 +1,16 @@
 import { ethers } from "ethers";
 import { promises as fs } from "fs";
 
+/*
+ * This script is used to create a new account.
+ * The address and private key of the account will be saved in a .address file.
+ */
 async function createAccount() {
   const wallet = ethers.Wallet.createRandom();
-
-  // Append the wallet address and private key in the .address file
-  await fs.appendFile(".address", wallet.address + "\t" + wallet.privateKey + "\n");
+  await fs.appendFile(
+    ".address",
+    wallet.address + "\t" + wallet.privateKey + "\n"
+  );
   console.log("Address and private key are saved in .address file");
 }
 
