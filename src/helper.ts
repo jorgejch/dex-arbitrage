d2494b7440880b77a221ee4420be4932c1bf2ac9
Jorge Haddad <jorgejch@gmail.com>
2024-11-21 00:54:58 -0800

    Remove unused helper.ts

--- src/helper.ts
+++
@@ -1,6 +0,0 @@
-async function fetchLiquidityInfo(tokenPair: string): Promise<any> {
-  // Logic to fetch liquidity information for the specified token pair
-  return {};
-}
-
-export { fetchLiquidityInfo };
