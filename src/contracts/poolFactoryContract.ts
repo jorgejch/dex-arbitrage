2a0a1332c8f1343d2c9ca7f4ac8288c8c876fef3
Jorge Haddad <jorgejch@gmail.com>
2024-11-21 01:56:48 -0800

    Rename PoolFactory to PoolFactoryContract and enhance pool fetching methods

--- src/contracts/poolFactoryContract.ts
+++ src/contracts/poolFactoryContract.ts
@@ -5,7 +5,7 @@ import { BaseContract } from "./baseContract";
  * Factory class that represents a contract instance
  * with methods to interact with it.
  */
-class PoolFactory extends BaseContract {
+class PoolFactoryContract extends BaseContract {
   /**
    * @param address The pool factory contract address
    * @param provider The WebSocket provider
@@ -20,16 +20,40 @@ class PoolFactory extends BaseContract {
   }
 
   /**
-   * Fetches the list of pools from the contract.
-   * @returns A list of pool addresses
+   * Fetches the pool address from the contract.
+   * 
+   * @param token0 The first token address
+   * @param token1 The second token address
+   * @returns The pool address
    */
-  public getPools() {
+  public async getPoolAddress(
+    token0: string,
+    token1: string
+  ): Promise<string | null> {
     if (!this._contract) {
-      this.createContract()
+      this.createContract();
     }
 
-    return this._contract?.getPools();
+    return await this._contract?.getPool(token0, token1);
+  }
+
+  /**
+   * Get a list of pools from the contract for a list of token pairs.
+   * 
+   * @param pairs A list of token pair addresses
+   * @returns A list of pool addresses
+   */
+  public async getPoolsAddresses(pairs: [token0: string, token1: string][]): Promise<string[]> {
+    const poolAddresses: string[] = [];
+    for (const [token0, token1] of pairs) {
+      const poolAddress = await this.getPoolAddress(token0, token1);
+      if (poolAddress) {
+        poolAddresses.push(poolAddress);
+      }
+    }
+
+    return poolAddresses;
   }
 }
 
-export { PoolFactory };
+export { PoolFactoryContract };
