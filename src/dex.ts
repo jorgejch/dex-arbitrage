1078b422cc9136061373b4dc7616a7af758d381f
Jorge Haddad <jorgejch@gmail.com>
2024-11-26 02:43:32 -0800

    Refactor Dex class to improve initialization and error handling, add WebSocketManager support, and enhance logging for pool contract creation

--- src/dex.ts
+++ src/dex.ts
@@ -1,35 +1,83 @@
+import { WebSocketManager } from "./ws.js";
 import { PSv3Subgraph } from "./subgraphs/psv3Subgraph.js";
 import { Pool } from "./types.js";
+import { PoolContract } from "./contracts/poolContract.js";
+import abi from "./abis/pancakeSwapv3PoolAbi.js";
+import { logger } from "./common.js";
 
 /**
  * Represents a DEX.
  */
 class Dex {
-  private pools: Map<string, Pool>;
   private inputTokenSymbolIndex: Map<string, Pool[]>;
+  private readonly contractsMap: Map<string, PoolContract>;
   private readonly subgraph: PSv3Subgraph;
-  private contracts: Map<string, any>;
+  private initialized: boolean;
+  private wsManager: WebSocketManager;
+  private pools: Pool[];
 
   /**
    * @param subgraph The Graph Subgraph instance
+   * @param wsManager WebSocket Manager
    */
-  constructor(subgraph: PSv3Subgraph) {
-    this.pools = new Map<string, Pool>();
+  constructor(subgraph: PSv3Subgraph, wsManager: WebSocketManager) {
     this.inputTokenSymbolIndex = new Map<string, Pool[]>();
     this.subgraph = subgraph;
+    this.initialized = false;
+    this.wsManager = wsManager;
+    this.contractsMap = new Map<string, PoolContract>();
+    this.pools = [];
   }
 
   /**
    * Initialize the DEX.
    */
   public async initialize(): Promise<void> {
-    this.subgraph.initialize();
+    try {
+      this.subgraph.initialize();
+    } catch (error) {
+      logger.error(
+        `Error initializing subgraph: ${error}`,
+        this.constructor.name
+      );
+      throw error;
+    }
 
-    const pools = await this.subgraph.getPools();
+    try {
+      this.pools = await this.subgraph.getPools();
+    } catch (error) {
+      logger.error(`Error fetching pools: ${error}`, this.constructor.name);
+      throw error;
+    }
 
-    for (const pool of pools) {
-      this.pools.set(pool.id, pool);
+    logger.info(`Fetched ${this.pools.length} pools`, this.constructor.name);
 
+    let poolCount = 0;
+
+    for (const pool of this.pools) {
+      logger.debug(`Creating pool # ${++poolCount}`, this.constructor.name);
+
+      // Create and store a PoolContract instance for each pool
+      const poolContract = new PoolContract(pool.id, this.wsManager, abi);
+      this.contractsMap.set(pool.id, poolContract);
+
+      // Initialize the contract instance
+      try {
+        poolContract.initialize();
+      } catch (error) {
+        logger.error(
+          `Error initializing pool contract: ${error}`,
+          this.constructor.name
+        );
+        throw error;
+      }
+
+      logger.info(
+        `Initialized pool for contract: ${pool.id}`,
+        this.constructor.name
+      );
+
+      // For each token in the pool, add the pool to the inputTokenSymbolIndex
       for (const token of pool.inputTokens) {
         if (!this.inputTokenSymbolIndex.has(token.symbol)) {
           this.inputTokenSymbolIndex.set(token.symbol, []);
@@ -37,13 +85,20 @@ class Dex {
         this.inputTokenSymbolIndex.get(token.symbol)!.push(pool);
       }
     }
+
+    this.initialized = true;
+    logger.info("Initialized Dex", this.constructor.name);
+  }
+
+  public isInitialized(): boolean {
+    return this.initialized;
   }
 
   /**
    * Get a list of pool contract addresses.
    */
   public getPoolAddresses(): string[] {
-    return Array.from(this.pools.keys());
+    return this.pools.map((pool) => pool.id);
   }
 
   /**
