685dea9ab6953a3fec4828e5edad105f570e533b
Jorge Haddad <jorgejch@gmail.com>
2024-11-25 17:31:24 -0800

    Refactor Controller class to integrate PSv3Subgraph and update environment variable handling

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,56 @@
-import { WebSocketManager } from "./ws";
+import { WebSocketManager } from "./ws.js";
+import { Dex } from "./dex.js";
+import { PoolFactoryContract } from "./contracts/poolFactoryContract.js";
+import { config } from "./common.js";
+import { PSv3Subgraph } from "./subgraphs/psv3Subgraph.js";
+
 import { ethers } from "ethers";
 import dotenv from "dotenv";
 dotenv.config();
 
 // Load environment variables
-const WSS_URL = process.env.QUICKNODE_WSS_PROVIDER ?? "";
-const HTTP_URL = process.env.QUICKNODE_HTTP_PROVIDER ?? "";
-const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY ?? "";
-const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS ?? "";
-const SIMULATE_DISCONNECT = process.env.SIMULATE_WSS_DISCONNECT === "true";
+const WSS_PROVIDER_URL: string = process.env.FAST_RPC_WSS_ENDPOINT ?? "";
+const HTTP_PROVIDER_URL: string = process.env.FAST_RPC_HTTP_ENDPOINT ?? "";
+const WALLET_PRIVATE_KEY: string = process.env.WALLET_PRIVATE_KEY ?? "";
+const AFLAB_CONTRACT_ADDRESS: string = process.env.CONTRACT_ADDRESS ?? "";
+const SIMULATE_DISCONNECT: boolean =
+  process.env.SIMULATE_WSS_DISCONNECT === "true";
+const THE_GRAPH_BASE_URL: string = process.env.THE_GRAPH_BASE_URL ?? "";
+const PANCAKESWAP_V3_SUBGRAPH_NAME: string =
+  process.env.THE_GRAPH_PANCAKESWAP_V3_SUBGRAPH_NAME ?? "";
 
 /**
  * Controller class that scans for arbitrage opportunities and triggers smart contract execution.
  */
 class Controller {
-  private readonly provider: ethers.JsonRpcProvider;
+  private readonly httpProvider: ethers.JsonRpcProvider;
+  private readonly wsProviderUrl: string;
   private readonly wallet: ethers.Wallet;
-  private readonly wsUrl: string;
-  private readonly contractAddress: string;
+  private readonly aflabContractAddress: string;
+  private readonly simulateDisconnect: boolean;
+  private readonly dex: Dex;
 
   constructor(
-    providerUrl: string,
-    privateKey: string,
-    wsUrl: string,
-    contractAddress: string
+    httpProviderUrl: string,
+    wsProviderUrl: string,
+    walletPrivateKey: string,
+    aflabContractAddress: string,
+    theGraphBaseUrl: string,
+    pancakeswapV3SubgraphName: string,
+    simulateDisconnect: boolean
   ) {
-    this.provider = new ethers.JsonRpcProvider(providerUrl);
-    this.wallet = new ethers.Wallet(privateKey, this.provider);
-    this.wsUrl = wsUrl;
-    this.contractAddress = contractAddress;
+    this.httpProvider = new ethers.JsonRpcProvider(httpProviderUrl);
+    this.wsProviderUrl = wsProviderUrl;
+    this.wallet = new ethers.Wallet(walletPrivateKey, this.httpProvider);
+    this.aflabContractAddress = aflabContractAddress;
+    this.simulateDisconnect = simulateDisconnect;
+
+    const subgraph = new PSv3Subgraph(
+      theGraphBaseUrl,
+      pancakeswapV3SubgraphName
+    );
+
+    this.dex = new Dex(subgraph);
   }
 
   /**
@@ -36,22 +58,18 @@ class Controller {
    */
   public start() {
     const wsManager = new WebSocketManager(
-      this.wsUrl,
-      [{ name: "block", handler: this.handleTradeData.bind(this) }],
-      SIMULATE_DISCONNECT
+      this.wsProviderUrl,
+      this.setUpListeners.bind(this),
+      this.simulateDisconnect
     );
+
+    // Start listening
     wsManager.start();
   }
-  private handleTradeData(data: any): void {
-    const tradeData = JSON.parse(data);
-    // Process trade data and scan for arbitrage opportunities
-    this.scanForArbitrageOpportunities(tradeData)
-      .then(() => {
-        console.log("Scanning for arbitrage opportunities...");
-      })
-      .catch((error) => {
-        console.error("Error scanning for arbitrage opportunities:", error);
-      });
+
+  // TODO: Implement the setUpListeners method
+  private setUpListeners(webSocketManager: WebSocketManager) {
+    return;
   }
 
   private async scanForArbitrageOpportunities(tradeData: any) {
@@ -106,28 +124,36 @@ class Controller {
   }
 }
 function main() {
-  // Test env vars are set
-  if (!HTTP_URL) {
-    throw new Error("QUICKNODE_HTTP_URL is not set");
+  if (!HTTP_PROVIDER_URL) {
+    throw new Error("HTTP_PROVIDER_URL is not set");
   }
   if (!WALLET_PRIVATE_KEY) {
     throw new Error("WALLET_PRIVATE_KEY is not set");
   }
-  if (!WSS_URL) {
-    throw new Error("QUICKNODE_WSS_URL is not set");
+  if (!WSS_PROVIDER_URL) {
+    throw new Error("WSS_PROVIDER_URL is not set");
   }
-  if (!CONTRACT_ADDRESS) {
-    throw new Error("CONTRACT_ADDRESS is not set");
+  if (!AFLAB_CONTRACT_ADDRESS) {
+    throw new Error("AFLAB_CONTRACT_ADDRESS is not set");
+  }
+  if (!THE_GRAPH_BASE_URL) {
+    throw new Error("THE_GRAPH_API_KEY is not set");
+  }
+  if (!PANCAKESWAP_V3_SUBGRAPH_NAME) {
+    throw new Error("PANCAKESWAP_V3_SUBGRAPH_NAME is not set");
   }
   if (SIMULATE_DISCONNECT) {
     console.log("Simulating WebSocket disconnections");
   }
 
   const controller = new Controller(
-    HTTP_URL,
+    HTTP_PROVIDER_URL,
+    WSS_PROVIDER_URL,
     WALLET_PRIVATE_KEY,
-    WSS_URL,
-    CONTRACT_ADDRESS
+    AFLAB_CONTRACT_ADDRESS,
+    THE_GRAPH_BASE_URL,
+    PANCAKESWAP_V3_SUBGRAPH_NAME,
+    SIMULATE_DISCONNECT
   );
   controller.start();
   console.log("Controller scanning for arbitrage opportunities...");
