e2d588b89b673cef7f13f494dc0c5beaed5331d5
Jorge Haddad <jorgejch@gmail.com>
2024-11-25 17:28:04 -0800

    Add TypeScript type definitions for Token, Fee, HourlySnapshot, and Pool objects

---
+++ src/types.ts
@@ -0,0 +1,59 @@
+/**
+ * Token object
+ * @typedef {Object} Token
+ * @property {string} id The token ID
+ * @property {string} name The token name
+ * @property {string} symbol The token symbol
+ * @property {number} decimals The token decimals
+ */
+type Token = {
+  id: string;
+  name: string;
+  symbol: string;
+  decimals: number;
+};
+
+/**
+ * Fee object
+ * @typedef {Object} Fee
+ * @property {string} feePercentage The fee percentage
+ * @property {string} feeType The fee type
+ */
+type Fee = {
+  feePercentage: string;
+  feeType: string;
+};
+
+/**
+ * Hourly snapshot object
+ * @typedef {Object} HourlySnapshot
+ * @property {number} hourlySwapCount The hourly swap count
+ * @property {number} hourlyVolumeUSD The hourly volume in USD
+ * @property {number} timestamp The timestamp of the hourly snapshot
+ */
+type HourlySnapshot ={
+  hourlySwapCount: number;
+  hourlyVolumeUSD: number;
+  timestamp: number;
+}
+
+/**
+ * Pool object
+ * @typedef {Object} Pool
+ * @property {string} id The pool ID
+ * @property {Object} pool The pool object
+ * @property {string} pool.name The pool name
+ * @property {string} pool.symbol The pool symbol
+ * @property {Array<Fee>} pool.fees The pool fees
+ * @property {Array<Token>} pool.inputTokens The pool input tokens
+ */
+type Pool = {
+  id: string;
+  name: string;
+  symbol: string;
+  inputTokens: Token[];
+  fees: Fee[];
+  hourlySnapshots: HourlySnapshot[];
+};
+
+export { Pool, Token, Fee, HourlySnapshot };
