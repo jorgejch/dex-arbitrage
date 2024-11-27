528ad94582961966efe9c28891f084837df3b825
Jorge Haddad <jorgejch@gmail.com>
2024-11-26 18:42:59 -0800

    Enhance logging and price impact handling: add log level configuration, implement price impact significance check, and introduce conversion function for Q64.96 fixed-point numbers

--- src/common.ts
+++ src/common.ts
@@ -1,4 +1,3 @@
-import poolFactoryAbi from "./abis/pancakeSwapV3FactoryAbi.js";
 import poolAbi from "./abis/pancakeSwapv3PoolAbi.js";
 import aflabAbi from "./abis/flashLoanArbitrageAbi.js";
 
@@ -7,14 +6,14 @@ import aflabAbi from "./abis/flashLoanArbitrageAbi.js";
  */
 const config = {
   LOGGER_PREFIX: "FlashLoanArbitrage",
-  POOL_FACTORY_ADDRESS: "0x0BFbCF9fa4f9C56B0F40a671Ad40E0805A091865",
-  POOL_FACTORY_ABI: poolFactoryAbi,
   POOL_ABI: poolAbi,
   AFLAB_ABI: aflabAbi,
+  LOG_LEVEL: "INFO",
   RECONNECT_INTERVAL_BASE: 1000, // Base interval for WSS reconnection attempts in milliseconds
   EXPECTED_PONG_BACK: 5000, // Time to wait for a pong response in milliseconds
   KEEP_ALIVE_CHECK_INTERVAL: 7500, // Interval for sending ping messages in milliseconds
   SIMULATE_DISCONNECT_INTERVAL: 15000, // Interval to simulate disconnections in milliseconds
+  PRICE_IMPACT_THRESHOLD: 3, // Price impact threshold for arbitrage opportunities in bps
 };
 
 /**
@@ -43,32 +42,62 @@ function exponentialBackoffDelay(
 }
 
 /**
+ * Converts a Q64.96 fixed-point number to a BigInt representing the price.
+ *
+ * @param sqrtPriceX96 The Q64.96 fixed-point number representing the square root of the price
+ * @returns The price as a BigInt
+ */
+function convertSqrtPriceX96ToBigInt(sqrtPriceX96: bigint): bigint {
+  const priceBigInt = sqrtPriceX96 ** BigInt(2) / BigInt(2) ** BigInt(96);
+  return priceBigInt;
+}
+
+/**
+ * Check if a price impact is significant.
+ *
+ * @param priceImpact The price impact in bps
+ */
+function isPriceImpactSignificant(priceImpact: bigint): boolean {
+  const threshold = config.PRICE_IMPACT_THRESHOLD;
+  return (priceImpact < BigInt(0) ? -priceImpact : priceImpact) >= threshold;
+}
+
+/**
  * Logger class.
  */
 class Logger {
   private readonly prefix: string;
+  private logLevel: string;
 
   /**
    * @param prefix The logger prefix
    */
-  constructor(prefix: string) {
+  constructor(prefix: string, logLevel: string = "INFO") {
     this.prefix = prefix;
+    this.logLevel = logLevel.toUpperCase();
   }
 
   private formatMessage(message: string, extraPrefix?: string): string {
-    const prefix = extraPrefix ? `[${this.prefix}][${extraPrefix}]` : `[${this.prefix}]`;
+    const prefix = extraPrefix
+      ? `[${this.prefix}][${extraPrefix}]`
+      : `[${this.prefix}]`;
     return `${prefix}${message}`;
   }
 
   public debug(message: string, extraPrefix?: string): void {
-    console.debug(this.formatMessage(`[DEBUG] ${message}`, extraPrefix));
+    if (!["INFO", "WARN", "ERROR"].includes(this.logLevel)) {
+      console.debug(this.formatMessage(`[DEBUG] ${message}`, extraPrefix));
+    }
   }
 
   public info(message: string, extraPrefix?: string): void {
-    console.log(this.formatMessage(`[INFO] ${message}`, extraPrefix));
+    if (!["WARN","ERROR"].includes(this.logLevel)) {
+      console.log(this.formatMessage(`[INFO] ${message}`, extraPrefix));
+    }
   }
 
   public warn(message: string, extraPrefix?: string): void {
+    if (this.logLevel === "ERROR") return;
     console.warn(this.formatMessage(`[WARN] ${message}`, extraPrefix));
   }
 
@@ -77,6 +106,13 @@ class Logger {
   }
 }
 
-const logger = new Logger(config.LOGGER_PREFIX);
+const logger = new Logger(config.LOGGER_PREFIX, config.LOG_LEVEL);
 
-export { config, getTGPancakeSwapUrl as getTGPancakeSwapMessariUrl, exponentialBackoffDelay, logger };
+export {
+  config,
+  getTGPancakeSwapUrl,
+  exponentialBackoffDelay,
+  isPriceImpactSignificant,
+  convertSqrtPriceX96ToBigInt,
+  logger,
+};
