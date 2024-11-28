5d833c4ae08c7127a20ea40380a812fb8480f724
Jorge Haddad <jorgejch@gmail.com>
2024-11-28 02:56:54 -0800

    Refactor start.ts: integrate dotenv for environment variable management, update subgraph name variable, and enhance error handling in main function

--- src/start.ts
+++ src/start.ts
@@ -1,5 +1,8 @@
 import { logger } from "./common.js";
 import { Controller } from "./controller.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // Load environment variables
 const WSS_PROVIDER_URL: string = process.env.FAST_RPC_WSS_ENDPOINT ?? "";
@@ -10,11 +13,12 @@ const SIMULATE_DISCONNECT: boolean =
   process.env.SIMULATE_WSS_DISCONNECT === "true";
 const THE_GRAPH_BASE_URL: string = process.env.THE_GRAPH_BASE_URL ?? "";
 const PANCAKESWAP_V3_SUBGRAPH_NAME: string =
-  process.env.THE_GRAPH_PANCAKESWAP_V3_SUBGRAPH_NAME ?? "";
+  process.env.THE_GRAPH_PANCAKESWAP_V3_SUBGRAPH_ID ?? "";
+const THE_GRAPH_API_KEY: string = process.env.THE_GRAPH_API_KEY ?? "";
 
 let controller: Controller;
 
-function main() {
+async function main() {
   if (!HTTP_PROVIDER_URL) {
     throw new Error("HTTP_PROVIDER_URL is not set");
   }
@@ -33,6 +37,9 @@ function main() {
   if (!PANCAKESWAP_V3_SUBGRAPH_NAME) {
     throw new Error("PANCAKESWAP_V3_SUBGRAPH_NAME is not set");
   }
+  if (!THE_GRAPH_API_KEY) {
+    throw new Error("THE_GRAPH_API_KEY is not set");
+  }
   if (SIMULATE_DISCONNECT) {
     console.log("Simulating WebSocket disconnections");
   }
@@ -43,12 +50,13 @@ function main() {
     WALLET_PRIVATE_KEY,
     AFLAB_CONTRACT_ADDRESS,
     THE_GRAPH_BASE_URL,
+    THE_GRAPH_API_KEY,
     PANCAKESWAP_V3_SUBGRAPH_NAME,
     SIMULATE_DISCONNECT
   );
 
   try {
-    controller.start();
+    await controller.start();
     logger.info("Controller scanning for arbitrage opportunities...");
   } catch (error) {
     logger.error(`Error running Controller: ${error}`);
@@ -57,7 +65,7 @@ function main() {
 }
 
 try {
-  main();
+  await main();
 } catch (error) {
   logger.error(`Error starting Controller: ${error}`);
 }
