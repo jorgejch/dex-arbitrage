9456769e6ba6bf622d2cca8a97d1f25caa0f63d5
Jorge Haddad <jorgejch@gmail.com>
2024-11-29 19:07:51 -0800

    Refactor Controller: enhance documentation, improve initialization logging, and integrate AflabContract

--- src/controller.ts
+++ src/controller.ts
@@ -2,12 +2,31 @@ import { WebSocketManager } from "./ws.js";
 import { BaseDex } from "./dexes/baseDex.js";
 import { PSv3Dex } from "./dexes/psv3Dex.js";
 import { DexPoolSubgraph } from "./subgraphs/dexPoolSubgraph.js";
-import { getTGPancakeSwapUrl, logger } from "./common.js";
+import { getTGPancakeSwapUrl, logger, config } from "./common.js";
+import { AflabContract } from "./contracts/aflabContract.js";
 
 import { ethers } from "ethers";
 
 /**
- * Controller class that scans for arbitrage opportunities and triggers smart contract execution.
+ * The `Controller` class orchestrates the initialization and management of decentralized exchanges (DEXes),
+ * WebSocket connections, and interactions with smart contracts. It handles the setup of providers, wallets,
+ * DEX instances, and manages their lifecycle.
+ *
+ * @class Controller
+ *
+ * @param httpProviderUrl - The URL of the HTTP provider for Ethereum JSON-RPC.
+ * @param wsProviderUrl - The URL of the WebSocket provider.
+ * @param walletPrivateKey - The private key of the wallet used for signing transactions.
+ * @param aflabContractAddress - The address of the AFLAB smart contract.
+ * @param theGraphBaseUrl - The base URL for The Graph API.
+ * @param theGraphApiKey - The API key for The Graph.
+ * @param pancakeswapV3SubgraphName - The subgraph name for PancakeSwap V3 on The Graph.
+ * @param simulateDisconnect - A boolean flag to simulate WebSocket disconnections (default is `false`).
+ *
+ * @method start - Initializes the Controller and starts the DEXes.
+ * @method stop - Stops the WebSocket manager.
+ *
+ * @returns An instance of the `Controller` class.
  */
 class Controller {
   private readonly httpProvider: ethers.JsonRpcProvider;
@@ -26,15 +45,6 @@ class Controller {
     pancakeswapV3SubgraphName: string,
     simulateDisconnect: boolean = false
   ) {
-    logger.info(
-      `Initializing Controller with arguments:
-      \tAFLAB contract Address: ${aflabContractAddress}
-      \tThe Graph Base URL: ${theGraphBaseUrl}
-      \tPS v3 Subgraph name: ${pancakeswapV3SubgraphName}
-      \tSimulate disconnect: ${simulateDisconnect}`,
-      this.constructor.name
-    );
-
     try {
       this.httpProvider = new ethers.JsonRpcProvider(httpProviderUrl);
       this.wsManager = new WebSocketManager(wsProviderUrl, simulateDisconnect);
@@ -43,12 +53,20 @@ class Controller {
       this.dexes = [
         new PSv3Dex(
           this.wsManager,
+          this.wallet,
           new DexPoolSubgraph(
             getTGPancakeSwapUrl(
               theGraphBaseUrl,
               pancakeswapV3SubgraphName,
               theGraphApiKey
             )
+          ),
+          new AflabContract(
+            this.aflabContractAddress,
+            config.AFLAB_ABI,
+            this.wsManager,
+            this.httpProvider,
+            this.wallet
           )
         ),
       ];
@@ -59,18 +77,20 @@ class Controller {
       );
       throw error;
     }
-  }
 
-  public getDexes(): BaseDex[] {
-    return this.dexes;
-  }
-
-  public getWSManager(): WebSocketManager {
-    return this.wsManager;
+    logger.info(
+      `Initialized Controller with arguments:
+      \tPSv3 AFLAB contract Address: ${aflabContractAddress}
+      \tThe Graph Base URL: ${theGraphBaseUrl}
+      \tPS v3 Subgraph name: ${pancakeswapV3SubgraphName}
+      \tWallet Address: ${this.wallet.address}
+      \tSimulate disconnect: ${simulateDisconnect}`,
+      this.constructor.name
+    );
   }
 
   /**
-   * Initializes the Controller.
+   * Starts the Controller.
    */
   public async start() {
     try {
@@ -96,6 +116,10 @@ class Controller {
       return;
     }
   }
+
+  /**
+   * Stops the Controller.
+   */
   public stop() {
     this.wsManager.stop();
   }
