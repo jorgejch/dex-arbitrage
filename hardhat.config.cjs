023d4cbb1aeece442064ac096b1c9f4c1010e011
Jorge Haddad <jorgejch@gmail.com>
2024-12-03 14:13:34 -0800

    Refactor hardhat configuration: streamline optimizer settings and remove unused deployment modules for aflab

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -8,75 +8,53 @@ module.exports = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200
-          }
-        }
+            runs: 200,
+          },
+        },
       },
       {
         version: "0.8.0",
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200
-          }
-        }
+            runs: 200,
+          },
+        },
       },
       {
         version: "0.8.20",
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200
-          }
-        }
+            runs: 200,
+          },
+        },
       },
       {
         version: "0.8.27",
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200
-          }
-        }
-      }
-    ]
+            runs: 200,
+          },
+        },
+      },
+    ],
   },
   defaultNetwork: "localhost",
   networks: {
     localhost: {
-      url: "http://127.0.0.1:8545"
+      url: "http://127.0.0.1:8545",
     },
     hardhat: {},
-    bsc: {
-      url: vars.get("QUICKNODE_BSC_HTTP_PROVIDER"),
-      chainId: 56,
-      accounts: [
-        vars.get("BSC_PRIVATE_KEY"),
-      ],
-    },
-    sepolia: {
-      url: vars.get("QUICKNODE_SEPOLIA_HTTP_PROVIDER"),
-      chainId: 11155111,
-      accounts: [
-        vars.get("SEPOLIA_PRIVATE_KEY"),
-      ],
-    },
-    bsctestnet: {
-      url: vars.get("QUICKNODE_BSCTESTNET_HTTP_PROVIDER"),
-      chainId: 97,
-      accounts: [
-        vars.get("BSCTESTNET_PRIVATE_KEY"),
-      ],
-    },
   },
-  sourcify:{
-    enabled: true
+  sourcify: {
+    enabled: true,
   },
-  etherscan:{
-    apiKey: vars.get("ETHERSCAN_API_KEY")
+  etherscan: {
+    apiKey: vars.get("ETHERSCAN_API_KEY"),
   },
   paths: {
     sources: "contracts",
-    tests: "tests/unit"
-  }
+  },
 };
