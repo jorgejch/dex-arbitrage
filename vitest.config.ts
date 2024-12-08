af2554393d4cdfc5f3e8b928c3db85ffb2592404
Jorge Haddad <jorgejch@gmail.com>
2024-12-07 16:54:22 -0800

    Add unit tests for BaseContract class and mock implementations

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,6 @@ const config: ViteUserConfig = {
   test: {
     coverage: {
       reporter: ["cobertura"],
-      include: ["src/**/*"],
       exclude: [
         "src/abis/**/*",
         "diagrams/**/*",
